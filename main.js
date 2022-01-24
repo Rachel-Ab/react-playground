ef323fc5c5c363101452d37e1033c8b4a605dd13
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2022-01-24 11:00:48 +0100

    exo 2 done

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const helloWorld = React.createElement('h1', {}, 'Hello world!');
+const helloWorld = <h1>Hello World !</h1>;
 
-
-// mauvaise pratique
-
-
-//ReactDOM.render(helloWorld, document.body);
-
-// Meilleure pratique mais pas encore LA meilleure
-
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(helloWorld, document.querySelector("#app"));
