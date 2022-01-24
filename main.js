923c66f020284c7ec6c6ef1930382bfec3b2dc81
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2022-01-24 11:49:54 +0100

    exo 3 done

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const helloWorld = <h1>Hello world!</h1>;
+/* const helloWorld = <h1>Hello world!</h1>; */
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+const lastName = 'abenzoar';
+const firstName = 'rachel';
+
+const UpperCase = (props) => {
+    return props.string.toUpperCase();
+};
+
+const firstUpperCase = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+const display = (
+    <div>
+    {/* <h1>{upperCase('hello')}</h1> */}
+    <UpperCase string="My naMe Is "/>
+    <span id='firstName'>{firstName.charAt(0).toUpperCase() + firstName.slice(1)} </span>
+    <p>{firstUpperCase(firstName)}</p>
+    <span id='lastName'>{lastName.toUpperCase()}</span>
+    </div>
+   
+)
+ReactDOM.render(display, document.querySelector('#app'));
