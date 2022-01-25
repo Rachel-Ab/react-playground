0a6a0d788898ddd23406d0eeb78b424745d40db4
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2022-01-25 12:19:37 +0100

    exo 8 done

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
-function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
-  }
-  
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
-  }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
-      }
-
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
-
-    return(
-        <React.Fragment>
-            {isLoggedIn ? 
-            <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
-            <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
-        </React.Fragment>
-    )
-  }
-  
-  ReactDOM.render(
-    <Greeting />,
-    document.querySelector('#app')
+function FetchingCard() {
+  const [card, setCard] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setCard(json));
+  }, []);
+  return (
+    <div className='card'>
+      {card.map((user) => {
+        return (
+          <div>
+            <Cards user={user} />
+          </div>
+        );
+      })}
+    </div>
   );
+}
+function Cards(props) {
+  const { user } = props;
+  return (
+    <ul className="list">
+      <li key={user.usename}>Name : {user.name}</li>
+      <li key={user.usename}>Email : {user.email}</li>
+      <li key={user.usename}>Phone : {user.phone}</li>
+      <li key={user.usename}>Website : {user.website}</li>
+      <li key={user.usename}>Company : {user.company.name}</li>
+    </ul>
+  );
+}
+ReactDOM.render(<FetchingCard />, document.querySelector("#app"));
