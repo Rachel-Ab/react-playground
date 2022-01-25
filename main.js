dc3352ae9e4777fae1b0c616a2720afe7401b3f2
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2022-01-25 10:38:27 +0100

    exo 7 done

--- main.js
+++ main.js
@@ -1,63 +1,33 @@
-function Clock(props) {
-    React.useEffect(() => {
-        tick();
-    }, []);
-    
-    const [date, setDate] = React.useState(new Date());
-    const [textColor, setTextColor] = React.useState("black");
-    const intervalRef = React.useRef();
-    const tick = () => {
-        intervalRef.current = setInterval(() => {
-            setDate(new Date());
-        }, 1000)
-        
-    }
-
-    const changeColor = () => {
-        var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
-        setTextColor(randomColor);
-    }
-
-    const resetColor = () => {
-        setTextColor('black')
-    }
-
-    const stopClock = (e) => {
-        e.preventDefault();
-        clearInterval(intervalRef.current);
-    }
-
-
-    const restartClock = (e) => {
-        e.preventDefault();
-        tick();
-    }
-    
-
-/*     return (
-        <div>
-            <h1>Hello world</h1>
-            <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-            <button onClick={changeColor}>Changer couleur</button>
-            <button onClick={resetColor}>Réinitialiser couleur</button>
-        </div>
-        ); */
-
-        // Solution Bonus
-
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
-                <button onClick={stopClock}>Stop</button>
-                <button onClick={restartClock}>Reprendre</button>
-            </div>
-            );
+function UserGreeting(props) {
+  return <h1>Bienvenue !</h1>;
 }
 
-ReactDOM.render(<Clock />, document.querySelector('#app'));
+function GuestGreeting(props) {
+  return <h1>Veuillez vous inscrire.</h1>;
+}
 
+function Greeting() {
+  const [isLoggedIn, setLogin] = React.useState(false);
 
+  const log = () => {
+    setLogin(!isLoggedIn);
+  };
 
+  return (
+    <div>
+      {isLoggedIn ? (
+        <React.Fragment>
+          <UserGreeting />
+          <button onClick={log}>Logout</button>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <GuestGreeting />
+          <button onClick={log}>Login</button>
+        </React.Fragment>
+      )}
+    </div>
+  );
+}
 
-
+ReactDOM.render(<Greeting />, document.getElementById("app"));
